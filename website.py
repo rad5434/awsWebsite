7c5fab0514808a976d1cfdbe65452c9f22f943fc
rad5434 <rad5434@gmail.com>
2018-04-25 22:00:31 -0400

    Generate a new page for 3d

--- website.py
+++ website.py
@@ -6,9 +6,14 @@ from random import *
 from pymongo import MongoClient
 
 app=Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/Generate3D')
+def index():
+    return render_template('3d.html')
 
 if __name__ == '__main__':
     app.debug = True
