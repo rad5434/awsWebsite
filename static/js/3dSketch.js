c3f856f95fe3e4c4120fc13bab23f0ee90668ec9
rad5434 <rad5434@gmail.com>
2018-04-26 23:19:31 -0400

    added 100*1500 to the size of 3D drawing

--- static/js/3dSketch.js
+++ static/js/3dSketch.js
@@ -69,9 +69,9 @@ var sketch = function(p,num,drawingSketches){
                 if(len<sides[0].length){    //need to do 3 lines per iteration (trianlge). currently we only have 1
 
                     //p.line((vertices[0].x_data[len]/2)+200, (tempObject.y_data[len]/2)+300, (tempObject.x_data[len+1]/2)+200, (tempObject.y_data[len+1]/2)+300);
-                    p.line(vertices[0][sides[0][len]]*10+1500,vertices[1][sides[0][len]]*10+1500,vertices[2][sides[0][len]]*10+1500,vertices[0][sides[1][len]]*10+1500,vertices[1][sides[1][len]]*10+1500,vertices[2][sides[1][len]]*10+1500)
-                    p.line(vertices[0][sides[1][len]]*10+1500,vertices[1][sides[1][len]]*10+1500,vertices[2][sides[1][len]]*10+1500,vertices[0][sides[2][len]]*10+1500,vertices[1][sides[2][len]]*10+1500,vertices[2][sides[2][len]]*10+1500)
-                    p.line(vertices[0][sides[2][len]]*10+1500,vertices[1][sides[2][len]]*10+1500,vertices[2][sides[2][len]]*10+1500,vertices[0][sides[0][len]]*10+1500,vertices[1][sides[0][len]]*10+1500,vertices[2][sides[0][len]]*10+1500)
+                    p.line(vertices[0][sides[0][len]]*100+1500,vertices[1][sides[0][len]]*100+1500,vertices[2][sides[0][len]]*100+1500,vertices[0][sides[1][len]]*100+1500,vertices[1][sides[1][len]]*100+1500,vertices[2][sides[1][len]]*100+1500)
+                    p.line(vertices[0][sides[1][len]]*100+1500,vertices[1][sides[1][len]]*100+1500,vertices[2][sides[1][len]]*100+1500,vertices[0][sides[2][len]]*100+1500,vertices[1][sides[2][len]]*100+1500,vertices[2][sides[2][len]]*100+1500)
+                    p.line(vertices[0][sides[2][len]]*100+1500,vertices[1][sides[2][len]]*100+1500,vertices[2][sides[2][len]]*100+1500,vertices[0][sides[0][len]]*100+1500,vertices[1][sides[0][len]]*100+1500,vertices[2][sides[0][len]]*100+1500)
                     console.log(vertices[0][sides[0][len]]/2,vertices[1][sides[0][len]]/2,vertices[2][sides[0][len]]/2,vertices[0][sides[1][len]]/2,vertices[1][sides[1][len]]/2,vertices[2][sides[1][len]]/2);
                     console.log(vertices[0][sides[1][len]]/2,vertices[1][sides[1][len]]/2,vertices[2][sides[1][len]]/2,vertices[0][sides[2][len]]/2,vertices[1][sides[2][len]]/2,vertices[2][sides[2][len]]/2);
                     console.log(vertices[0][sides[2][len]]/2,vertices[1][sides[2][len]]/2,vertices[2][sides[2][len]]/2,vertices[0][sides[0][len]]/2,vertices[1][sides[0][len]]/2,vertices[2][sides[0][len]]/2);
