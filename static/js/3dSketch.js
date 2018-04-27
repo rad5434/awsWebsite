0829b78669c3bc1379278487e1feaca4663f7aa2
rad5434 <rad5434@gmail.com>
2018-04-27 12:09:39 -0400

    *40+400

--- static/js/3dSketch.js
+++ static/js/3dSketch.js
@@ -40,7 +40,6 @@ var sketch = function(p,num,drawingSketches){
     var vertices
     p.setup = function() {
         p.createCanvas(1200,1200);
-	    p.background(200)
         p.stroke(255);     // Set line drawing color to white
         p.frameRate(30);
         //console.log("Setup is finished");
@@ -63,10 +62,10 @@ var sketch = function(p,num,drawingSketches){
                 p.stroke(z);
                 if(len<sides[0].length){    //need to do 3 lines per iteration (trianlge). currently we only have 1
 
-                    //p.line(((vertices[0].x_data[len]/2)*40)+200, (tempObject.y_data[len]/2)+300, (t(empObject.x_data[len+1]/2)*40)+200, (tempObject.y_data[len+1]/2)+300);
-                    p.line((vertices[0][sides[0][len]]*40)+200,(vertices[1][sides[0][len]]*40)+200,(vertices[2][sides[0][len]]*40)+200,(vertices[0][sides[1][len]]*40)+200,(vertices[1][sides[1][len]]*40)+200,(vertices[2][sides[1][len]]*40)+200)
-                    p.line((vertices[0][sides[1][len]]*40)+200,(vertices[1][sides[1][len]]*40)+200,(vertices[2][sides[1][len]]*40)+200,(vertices[0][sides[2][len]]*40)+200,(vertices[1][sides[2][len]]*40)+200,(vertices[2][sides[2][len]]*40)+200)
-                    p.line((vertices[0][sides[2][len]]*40)+200,(vertices[1][sides[2][len]]*40)+200,(vertices[2][sides[2][len]]*40)+200,(vertices[0][sides[0][len]]*40)+200,(vertices[1][sides[0][len]]*40)+200,(vertices[2][sides[0][len]]*40)+200)
+                    //p.line(((vertices[0].x_data[len]/2)*40)+400, (tempObject.y_data[len]/2)+300, (t(empObject.x_data[len+1]/2)*40)+400, (tempObject.y_data[len+1]/2)+300);
+                    p.line((vertices[0][sides[0][len]]*40)+400,(vertices[1][sides[0][len]]*40)+400,(vertices[2][sides[0][len]]*40)+400,(vertices[0][sides[1][len]]*40)+400,(vertices[1][sides[1][len]]*40)+400,(vertices[2][sides[1][len]]*40)+400)
+                    p.line((vertices[0][sides[1][len]]*40)+400,(vertices[1][sides[1][len]]*40)+400,(vertices[2][sides[1][len]]*40)+400,(vertices[0][sides[2][len]]*40)+400,(vertices[1][sides[2][len]]*40)+400,(vertices[2][sides[2][len]]*40)+400)
+                    p.line((vertices[0][sides[2][len]]*40)+400,(vertices[1][sides[2][len]]*40)+400,(vertices[2][sides[2][len]]*40)+400,(vertices[0][sides[0][len]]*40)+400,(vertices[1][sides[0][len]]*40)+400,(vertices[2][sides[0][len]]*40)+400)
                     len+=1;
                 }
                 else{
