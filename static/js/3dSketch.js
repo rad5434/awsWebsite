b6af6ffad80d7295158dd2042f3cfee1f2c1b4c5
rad5434 <rad5434@gmail.com>
2018-04-27 00:40:28 -0400

    *100+300

--- static/js/3dSketch.js
+++ static/js/3dSketch.js
@@ -62,10 +62,10 @@ var sketch = function(p,num,drawingSketches){
                 p.stroke(z);
                 if(len<sides[0].length){    //need to do 3 lines per iteration (trianlge). currently we only have 1
 
-                    //p.line((vertices[0].x_data[len]/2)*100+200, (tempObject.y_data[len]/2)+300, (tempObject.x_data[len+1]/2)*100+200, (tempObject.y_data[len+1]/2)+300);
-                    p.line(vertices[0][sides[0][len]]*100+200,vertices[1][sides[0][len]]*100+200,vertices[2][sides[0][len]]*100+200,vertices[0][sides[1][len]]*100+200,vertices[1][sides[1][len]]*100+200,vertices[2][sides[1][len]]*100+200)
-                    p.line(vertices[0][sides[1][len]]*100+200,vertices[1][sides[1][len]]*100+200,vertices[2][sides[1][len]]*100+200,vertices[0][sides[2][len]]*100+200,vertices[1][sides[2][len]]*100+200,vertices[2][sides[2][len]]*100+200)
-                    p.line(vertices[0][sides[2][len]]*100+200,vertices[1][sides[2][len]]*100+200,vertices[2][sides[2][len]]*100+200,vertices[0][sides[0][len]]*100+200,vertices[1][sides[0][len]]*100+200,vertices[2][sides[0][len]]*100+200)
+                    //p.line((vertices[0].x_data[len]/2)*100+300, (tempObject.y_data[len]/2)+300, (tempObject.x_data[len+1]/2)*100+300, (tempObject.y_data[len+1]/2)+300);
+                    p.line(vertices[0][sides[0][len]]*100+300,vertices[1][sides[0][len]]*100+300,vertices[2][sides[0][len]]*100+300,vertices[0][sides[1][len]]*100+300,vertices[1][sides[1][len]]*100+300,vertices[2][sides[1][len]]*100+300)
+                    p.line(vertices[0][sides[1][len]]*100+300,vertices[1][sides[1][len]]*100+300,vertices[2][sides[1][len]]*100+300,vertices[0][sides[2][len]]*100+300,vertices[1][sides[2][len]]*100+300,vertices[2][sides[2][len]]*100+300)
+                    p.line(vertices[0][sides[2][len]]*100+300,vertices[1][sides[2][len]]*100+300,vertices[2][sides[2][len]]*100+300,vertices[0][sides[0][len]]*100+300,vertices[1][sides[0][len]]*100+300,vertices[2][sides[0][len]]*100+300)
                     len+=1;
                 }
                 else{
