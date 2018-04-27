b3c3fe100769bbde263720d7613b003d777fc706
rad5434 <rad5434@gmail.com>
2018-04-26 23:45:43 -0400

    *10 to the size of 3D drawing

--- static/js/3dSketch.js
+++ static/js/3dSketch.js
@@ -66,9 +66,9 @@ var sketch = function(p,num,drawingSketches){
                 if(len<sides[0].length){    //need to do 3 lines per iteration (trianlge). currently we only have 1
 
                     //p.line((vertices[0].x_data[len]/2)+200, (tempObject.y_data[len]/2)+300, (tempObject.x_data[len+1]/2)+200, (tempObject.y_data[len+1]/2)+300);
-                    p.line(vertices[0][sides[0][len]],vertices[1][sides[0][len]],vertices[2][sides[0][len]],vertices[0][sides[1][len]],vertices[1][sides[1][len]],vertices[2][sides[1][len]])
-                    p.line(vertices[0][sides[1][len]],vertices[1][sides[1][len]],vertices[2][sides[1][len]],vertices[0][sides[2][len]],vertices[1][sides[2][len]],vertices[2][sides[2][len]])
-                    p.line(vertices[0][sides[2][len]],vertices[1][sides[2][len]],vertices[2][sides[2][len]],vertices[0][sides[0][len]],vertices[1][sides[0][len]],vertices[2][sides[0][len]])
+                    p.line(vertices[0][sides[0][len]]*10,vertices[1][sides[0][len]]*10,vertices[2][sides[0][len]]*10,vertices[0][sides[1][len]]*10,vertices[1][sides[1][len]]*10,vertices[2][sides[1][len]]*10)
+                    p.line(vertices[0][sides[1][len]]*10,vertices[1][sides[1][len]]*10,vertices[2][sides[1][len]]*10,vertices[0][sides[2][len]]*10,vertices[1][sides[2][len]]*10,vertices[2][sides[2][len]]*10)
+                    p.line(vertices[0][sides[2][len]]*10,vertices[1][sides[2][len]]*10,vertices[2][sides[2][len]]*10,vertices[0][sides[0][len]]*10,vertices[1][sides[0][len]]*10,vertices[2][sides[0][len]]*10)
                     len+=1;
                 }
                 else{
